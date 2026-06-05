8f78873cbebc58dec61fd04caef92dc75ef98329
B.K.Sandhiya <sandhiyabk1723@gmail.com>
2026-06-05 20:13:46 +0530

    Add explanation for hashmap usage in two_sum
    Added a comment explaining the use of hashmap for better solution.

--- Two_sum.py
+++ Two_sum.py
@@ -8,3 +8,4 @@ class Solution:
             seen[nums[i]]=i
 s=Solution()
 print(s.two_sum([2,7,11,15],9))
+#the hashmap is using so that we can have value and it's index like 2:0 7:1 4:2 for better solution by using formula (remain=target-nums[i]) and check if remain in hashmap.
