88192e443c7fdb5569e7e25b1b8ecba5f0f1893a
B.K.Sandhiya <sandhiyabk1723@gmail.com>
2026-06-13 18:15:37 +0530

    Implement topKFrequent method to find top k elements

---
+++ top_k_frequen_element.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        result = sorted(count, key=count.get, reverse=True)
+        return result[:k]
