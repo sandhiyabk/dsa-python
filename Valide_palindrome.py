ba3d786f68d5dfc35a5d7525420b3fcbd5aa894e
B.K.Sandhiya <sandhiyabk1723@gmail.com>
2026-06-15 20:18:18 +0530

    Implement palindrome checking in Solution class

---
+++ Valide_palindrome.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        left=0
+        right=len(s)-1
+        while left<right:
+            if s[left]==s[right]:
+                left+=1
+                right-=1
+            else:
+                return False
+        return True
