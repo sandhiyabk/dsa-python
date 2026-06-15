fe710fb0557b7a4977ee14727da7b049784dbc30
B.K.Sandhiya <sandhiyabk1723@gmail.com>
2026-06-15 20:21:52 +0530

    Enhance comment for string normalization
    Updated comment to clarify the purpose of the isalnum() method.

--- Valide_palindrome.py
+++ Valide_palindrome.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        s = ''.join(c.lower() for c in s if c.isalnum())
+        s = ''.join(c.lower() for c in s if c.isalnum())  #isalnum() for keeps only letters and digits and convert into lowercase if it is either char or num and finally join all together
         left=0
         right=len(s)-1
         while left<right:
