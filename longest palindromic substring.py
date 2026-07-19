f6641c686c405d21499f971030237578ad38c44a
B.K.Sandhiya <sandhiyabk1723@gmail.com>
2026-07-19 17:55:31 +0530

    Implement longest palindromic substring solution

---
+++ longest palindromic substring.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        if not s:
+            return ""
+        start, end = 0, 0
+        def expand_around_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return left + 1, right - 1
+        for i in range(len(s)):
+            l1, r1 = expand_around_center(i, i)
+            if (r1 - l1) > (end - start):
+                start, end = l1, r1
+            l2, r2 = expand_around_center(i, i + 1)
+            if (r2 - l2) > (end - start):
+                start, end = l2, r2
+        return s[start:end + 1]
